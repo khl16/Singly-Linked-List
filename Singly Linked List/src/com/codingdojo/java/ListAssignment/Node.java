d03302d3aa5bcd7e48603d547fc0c1b93ea6bccb
Khaled-Aleadini <103153347+khl16@users.noreply.github.com>
2022-07-24 04:10:47 +0300

    SinglyLinkedList

---
+++ Singly Linked List/src/com/codingdojo/java/ListAssignment/Node.java
@@ -0,0 +1,14 @@
+package com.codingdojo.java.ListAssignment;
+
+public class Node {
+	 
+	 public int value;
+	 public Node next;
+	    
+	    
+	    public Node(int value) {
+	    	this.value = value;
+	        this.next = null;
+
+	    }
+}
